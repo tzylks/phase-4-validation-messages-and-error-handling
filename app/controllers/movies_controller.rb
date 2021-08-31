035575771aa5a113344627a9ffa020f4d56fd7ad
TrevorZylks <trevorzylks@gmail.com>
2021-08-31 09:59:31 -0700

    Completed Assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,19 @@
 class MoviesController < ApplicationController
   wrap_parameters false
+
+  CATEGORIES = ['Comedy', 'Drama', 'Animation', 'Mystery', 'Horror', 'Fantasy', 'Action', 'Documentary', 'Science Fiction']
+
+  validates :title, presence: true
+  validates :year, numericality: {
+    greater_than_or_equal_to: 1888,
+    less_than_or_equal_to: Date.today.year
+  }
+  validates :poster_url, presence: true
+  validates :category, inclusion: {
+    in: CATEGORIES,
+    message: "must be one of: #{CATEGORIES.join(', ')}"
+  }
+
   
   def index
     movies = Movie.all
